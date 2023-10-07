cc0e993009afdb9de0a57bb3c371a13d2f03e487
Роман Семашкевич <semaroman@mail.ru>
2023-10-07 13:19:50 +0300

    Commit

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Client {
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", Server.PORT);
+             PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            pw.println("hello from Server");
+            System.out.println("This client is running on port: " + br.readLine());
+        } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static final Integer PORT = 8080;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    System.out.println("New connection accepted");
+                    final String name = in.readLine();
+                    out.println(String.format("Hi, %s, your port is %d", name, clientSocket.getPort()));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
